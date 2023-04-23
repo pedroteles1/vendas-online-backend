d567ae77e3d69054e57650c61728f1823125bd7d
Pedro Silva <pedro.teles@igcp.com.br>
2023-04-23 13:27:11 -0300

    :sparkles: another pc

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -3,10 +3,10 @@ import { CreateUserDto } from './dtos/createUser.dto';
 
 @Controller('user')
 export class UserController {
+
   @Post()
-  async createUser(
-    @Body() createUser: CreateUserDto, // variable createUser will store the data sent by the user in the body of the request
-  ) {
+  async createUser( @Body() createUser: CreateUserDto) {
+    // variable createUser will store the data sent by the user in the body of the request
     return {
       ...createUser,
       password: undefined, // the password will not be returned to the user
